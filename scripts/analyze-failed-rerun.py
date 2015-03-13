3e8fb47068f165ad022e423d65219ed89fe322cd
qianden <qianden@cisco.com>
2015-03-12 17:30:59 -0700

    send email with rerun tags

--- scripts/analyze-failed-rerun.py
+++ scripts/analyze-failed-rerun.py
@@ -2,6 +2,8 @@ import os,glob,errno,sys
 import shutil,stat
 import distutils.core
 import pdb
+import smtplib
+from email.mime.text import MIMEText
 
 wmepath=os.path.abspath(os.path.join(os.environ['WORKSPACE'], "..", "..", os.environ['repo_loc'], os.environ['wme_loc']))
 wx2testpath=os.path.join(wmepath,"ta")
@@ -21,10 +23,20 @@ def get_tag():
         tag=item.split(":")[-1]+","+tag
     tag=tag[:-1]
     print tag
+	
+def send_email(content,subject,to):
+	msg=MIMEText(content)
+	msg['Subject'] = subject
+	msg['To'] = to
+	msg['From'] = "jenkins@wme-jenkins.cisco.com"
+	s = smtplib.SMTP('outbound.cisco.com:25')
+	s.sendmail("jenkins@wme-jenkins.cisco.com", [to], msg.as_string())
+	s.quit()
     
 os.chdir(wx2testfeaturepath)
 rerun=int(os.environ['rerun_times'])
 linus=str(os.environ['linus_address'])
+rerun_tags="rerun tags: "
 
 while os.stat("rerun.txt").st_size != 0 and rerun>0:
 	tag=""
@@ -33,7 +45,12 @@ while os.stat("rerun.txt").st_size != 0 and rerun>0:
 	print "cucumber ta_features --tags %s --tags @sanity --format pretty --format json --out report.json --format rerun --out rerun.txt --format junit --out junit"%tag
 	os.system("cucumber ta_features --tags %s --tags @sanity --format pretty --format json --out report.json --format rerun --out rerun.txt --format junit --out junit LINUS_SERVER=%s"%(tag,linus))
 	rerun=rerun-1
+	rerun_tags+=tag+","
 
+bld_num=os.environ['parent_project']+os.environ['BUILD_NUMBER']
+subject="[Jenkins Alert] Attention: WIN TA rerun tags for build %s"%bld_num
+send_email(rerun_tags[:-1], subject, "wme-buildpipeline-scrum@cisco.com")
+	
 if os.stat("rerun.txt").st_size != 0: 
     tag=""
     get_tag()
