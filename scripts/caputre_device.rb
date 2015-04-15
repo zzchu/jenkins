28079c95fa49589216406f6e6e6431b3b8d7e18c
qianden <qianden@cisco.com>
2015-04-15 15:24:26 -0700

    update capture device

--- scripts/caputre_device.rb
+++ scripts/caputre_device.rb
@@ -5,6 +5,10 @@ require_relative './idevicehelpers.rb'
 
 device_udids = Idevice.device_list
 
+id_str=""
 device_udids.each do |device_udid|
 	$stdout.puts "Found ios device: #{device_udid}"
+	id_str+=device_udid+","
 end
+id_str=id_str[0..-1]
+File.open("ios_id", 'w') { |file| file.write(id_str) }
