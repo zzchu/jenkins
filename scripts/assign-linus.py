15fd7f07d2df61fd83165cf4819ad195154533f5
qianden <qianden@cisco.com>
2015-05-22 14:42:52 -0700

    fix syntax error

--- scripts/assign-linus.py
+++ scripts/assign-linus.py
@@ -7,13 +7,11 @@ win_linus_address = sjc_linus
 hz_linus = str(os.environ["hz_linus_address"])
 path = os.path.abspath(os.path.join(os.environ['WORKSPACE']))
 
-win_linus_address = sjc_linus
-if "SJC" not in node:
-	win_linus_address = str(hz_linus)
+win_linus_address = hz_linus if "SJC" not in node else sjc_linus
 print "[INFO] win linus address is %s"%win_linus_address
 
 os.chdir(path)
-filename = "propsfile.linus_addr."+os.environ["parent_project"]
-with open("filename", "w+") as text_file:
+filename = "propsfile.linus_addr.%s"%os.environ["parent_project"]
+with open(filename, "w+") as text_file:
     text_file.write("win_linus_address=%s" % win_linus_address)
 
